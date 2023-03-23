c39c6fae83aa6b17406a93a9e476d4c39223f730
CatalinTirdea <tirdea.catalin@yahoo.com>
2023-03-23 13:35:55 +0200

    added constructors to every class

--- main.cpp
+++ main.cpp
@@ -46,13 +46,14 @@ class DriverChampionship{
     return points;
   }
   
-  void setPoints (int point){
+  DriverChampionship(int point){
     points = point;
   }
   
   void addPoints (int point){
     points += point;
   }
+  friend class Driver;
 };
 
 class ConstructorChampionship{
@@ -70,7 +71,7 @@ class ConstructorChampionship{
   void addTeamPoints (int teamPoint){
     teamPoints += teamPoint;
   }
-
+  friend class Team;
 };
 
 class Tires{
@@ -86,17 +87,41 @@ class Tires{
     compound = comp;
    }
 
+   Tires(String comp) {
+    compound= comp;
+   }
+
+  friend class Races;
 };
 
 class Races{
     String date;
-    String circuiteType;
+    String circuitType;
     String fastestLap;
     Tires tire1;
     Tires tire2;
     Tires tire3;
     Circuits circuit;
+    
+  Races (String dateRace,String circType,Circuits circ) {
+   
+       date = dateRace;
+       circuitType = circType;
+       circuit = circ;
 
+   
+   
+}
+
+ Races (Tires t1,Tires t2,Tires t3){
+  tire1 = t1;
+  tire2 = t2;
+  tire3 = t3;
+ }
+
+ Races(String fl){
+  fastestLap = fl;
+ }
 };
 
 class Circuits
@@ -105,7 +130,17 @@ class Circuits
   double length;
   int numberOfCorners;
   int numberOfDRSZones;
-  
+ 
+  Circuits () :  length(0.0), numberOfCorners(0),numberOfDRSZones(0){}
+
+  Circuits (String nm, double lng, int nrcorners, int nrdrs){
+    name = nm;
+    length = lng;
+    numberOfCorners = nrcorners;
+    numberOfDRSZones = nrdrs;
+  }
+
+  friend class Races;
 
 };
 
@@ -114,7 +149,14 @@ class EngineManufacturers
   String manufacturerName;
   double HP;
   int cc;
- 
+  EngineManufacturers () : HP(0.0), cc(0){}
+
+  EngineManufacturers (String mnfname, double hp, int cylcap){
+    manufacturerName = mnfname;
+    HP = hp;
+    cc = cylcap;
+  }
+  friend class Car;
 };
 
 class Driver{
@@ -125,6 +167,25 @@ class Driver{
  String nationality;
  DriverChampionship points;
  int driverPenaltyPoints;
+ 
+ Driver() : age(0),weigth(0.0),height(0.0), driverPenaltyPoints(0){}
+ 
+ Driver(String nm, int ag,double wei,double hei,String nat){
+  name = nm;
+  age = ag;
+  weigth = wei;
+  hei = height;
+  nationality = nat;
+ }
+  
+  Driver(DriverChampionship pnts){
+    points = pnts;
+  }
+  
+  Driver(int pnltypoints){
+    driverPenaltyPoints = pnltypoints;
+  }
+  friend class Team;
 };
 
 
@@ -135,6 +196,20 @@ class Car
     EngineManufacturers engine;
     String color;
     
+    Car() :  length(0.0){}
+
+    Car(String nm, double lngth, EngineManufacturers eng, String clr){
+      name = nm;
+      length = lngth;
+      engine = eng;
+      color = clr;
+    } 
+
+    Car(EngineManufacturers eng){
+      engine = eng;
+    }
+
+    friend class Team;
 };
 
 class Team
@@ -147,9 +222,39 @@ class Team
   Car    car2;
   ConstructorChampionship teamPoints;
   String Sponsors;
-  int budget;
+  long int budget;
   int teamPenaltyPoints;
+
+  Team () : budget(140000000), teamPenaltyPoints(0){}
   
+  Team(String nm, TeamPrincipal tp, Driver d1, Car c1, Driver d2,Car c2, ConstructorChampionship teamp, String spns,long int bdg, int tmpnltypnts){
+    name = nm;
+    teamPrincipal = tp;
+    driver1 = d1;
+    car1 = c1;
+    driver2 = d2;
+    car2 = c2;
+    teamPoints = teamp;
+    Sponsors = spns;
+    budget = bdg;
+    teamPenaltyPoints = tmpnltypnts;
+
+  }
+
+  Team(Driver d2, Car c2)
+  {
+   driver1 = driver2;
+   car1 = car2;
+   driver2 =d2;
+   car2 =c2;
+     }
+  
+  Team(TeamPrincipal tp){
+    teamPrincipal = tp;
+  }
+  Team(String spns){
+    Sponsors = spns;
+  }
 };
 
 int main()
